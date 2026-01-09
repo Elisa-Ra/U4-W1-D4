a7ea8c17723978f7929c317006c3bbf6056a13ef
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-09 18:30:15 +0100

    adds audio

--- u4-w1-d4/src/entities/Audio.java
+++ u4-w1-d4/src/entities/Audio.java
@@ -1,4 +1,61 @@
+/* Una registrazione Audio è riproducibile, ha associato un int volume con i metodi
+ * abbassaVolume() e alzaVolume(). Se riprodotta -> play(), stampa il titolo un numero di volte
+ * pari alla durata insieme a un numero di punti esclamativi uguali al volume.
+ * Volume = punti esclamativi
+ * Titolo = durata */
+
 package entities;
 
-public class Audio {
+import interfaces.Riproducibile;
+
+public class Audio extends ElementoMultimediale implements Riproducibile {
+    private final int durata; // uso final perché non si deve poter cambiare la durata
+    private int volume;
+
+    public Audio(String title, int durata, int volume) {
+        super(title); // utilizzo il costruttore del padre per il titolo
+        this.durata = durata;
+        this.volume = 5;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int abbassaVolume(int newVolume) {
+        if (this.volume > newVolume) {
+            this.volume = newVolume;
+        } else {
+            System.out.println("Il volume è già più basso del numero inserito");
+        }
+        return this.volume;
+    }
+
+    public int alzaVolume(int newVolume) {
+        if (this.volume < newVolume) {
+            this.volume = newVolume;
+        } else {
+            System.out.println("Il volume è già più alto del numero inserito");
+        }
+        return this.volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo di media: Audio" +
+                ". Titolo: " + this.getTitle();
+    }
+
+    @Override
+    public void play() {
+        String puntiEsclamativi = ""; // la inizializzo vuota
+        // aggiungo un punto esclamativo n volte = volume
+        for (int i = 0; i < volume; i++) {
+            puntiEsclamativi += "!";
+        }
+        // stampo il titolo (con i punti esclamativi) n volte = durata
+        for (int i = 0; i < durata; i++) {
+            System.out.println("Titolo: " + getTitle() + puntiEsclamativi);
+        }
+    }
 }
--- u4-w1-d4/src/entities/ElementoMultimediale.java
+++ u4-w1-d4/src/entities/ElementoMultimediale.java
@@ -1,4 +1,20 @@
 package entities;
 
-public class ElementoMultimediale {
+// La mia classe astratta
+public abstract class ElementoMultimediale {
+    private String title;
+
+    // Gli elementi multimediali hanno un titolo
+    public ElementoMultimediale(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Titolo: " + title;
+    }
 }
