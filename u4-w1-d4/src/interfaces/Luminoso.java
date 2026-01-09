d36a08c58549063a0a4649bce2d8e15160696085
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-09 18:32:23 +0100

    adds interfaces

--- u4-w1-d4/src/interfaces/Luminoso.java
+++ u4-w1-d4/src/interfaces/Luminoso.java
@@ -1,4 +1,7 @@
 package interfaces;
 
 public interface Luminoso {
+    public void aumentaLuminosita();
+
+    public void diminuisciLuminosita();
 }
--- u4-w1-d4/src/interfaces/Visualizzabile.java
+++ u4-w1-d4/src/interfaces/Visualizzabile.java
@@ -1,4 +1,5 @@
 package interfaces;
 
 public interface Visualizzabile {
+    public void show();
 }
